21d2e6c4726f5c46ef576805fd58bc4b70fd44de
Marquis <511680713@qq.com>
2020-06-11 14:54:33 +0800

    [update]add SubscribeMessage

---
+++ src/MiniProgram/SubscribeMessage/ServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marquis
+ * Date: 2020/6/11
+ * Time: 下午2:47
+ */
+
+namespace EasyWeChat\MiniProgram\SubscribeMessage;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class ServiceProvider
+{
+    /**
+     * {@inheritdoc}.
+     */
+    public function register(Container $app)
+    {
+        $app['subscribe_message'] = function ($app) {
+            return new Client($app);
+        };
+    }
+}
